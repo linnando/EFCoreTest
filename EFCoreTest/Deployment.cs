b52c55e475f008ebaea5d192bc76a8907be7623a
Ruslan Batdalov <ruslan@bitreg.com>
2016-11-23 22:10:24 +0200

    IQueryable processing bug

---
+++ EFCoreTest/Deployment.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCoreTest
+{
+    [TestClass]
+    public class Deployment
+    {
+        [ClassInitialize]
+        public static void PrepareTests(TestContext testContext)
+        {
+            using (var context = new BloggingContext())
+            {
+                context.Database.Migrate();
+                if (!context.Blogs.Any(b => b.Url == "test"))
+                    context.Blogs.Add(new Blog {Url = "test"});
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestBlog()
+        {
+            using (var context = new BloggingContext())
+            {
+                var blog = context.Blogs.FirstOrDefault(b => b.Url == "test");
+                Assert.IsNotNull(blog);
+            }
+        }
+    }
+}
---
+++ EFCoreTest/MainTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCoreTest
+{
+    [TestClass]
+    public class MainTest
+    {
+        private static BloggingContext _context;
+        
+        [ClassInitialize]
+        public static void PrepareTests(TestContext testContext)
+        {
+            _context = new BloggingContext();
+        }
+
+        [TestMethod]
+        public void TestGetByQueryableUrl()
+        {
+            var blogs1 = GetBlogsByQueryableUrl(new[] { "test" }.AsQueryable());
+            System.Diagnostics.Debug.WriteLine(blogs1.Count());
+            var blogs2 = GetBlogsByQueryableUrl(new[] { "wrong_url" }.AsQueryable());
+            System.Diagnostics.Debug.WriteLine(blogs2.Count());
+        }
+
+        public IQueryable<Blog> GetBlogsByQueryableUrl(IQueryable<string> urls)
+        {
+            return _context.Blogs.Where(blog => urls.Contains(blog.Url));
+        }
+
+        [TestMethod]
+        public void TestGetByEnumerableUrl()
+        {
+            var blogs1 = GetBlogsByEnumerableUrl(new[] { "test" }.AsQueryable());
+            System.Diagnostics.Debug.WriteLine(blogs1.Count());
+            var blogs2 = GetBlogsByEnumerableUrl(new[] { "wrong_url" }.AsQueryable());
+            System.Diagnostics.Debug.WriteLine(blogs2.Count());
+        }
+
+        public IQueryable<Blog> GetBlogsByEnumerableUrl(IEnumerable<string> urls)
+        {
+            return _context.Blogs.Where(blog => urls.Contains(blog.Url));
+        }
+    }
+}
